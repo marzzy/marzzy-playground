766b167ce47635dcce86bdb38e889a3aadd34d9a
marzzy mogh <marzzy.m@gmail.com>
2020-12-17 14:52:29 +0330

    forget to commit the perv solution, but it works for uniq item, arr

--- algorithm/non-decreasing-array.js
+++ algorithm/non-decreasing-array.js
@@ -1,34 +1,46 @@
 //LINKS: https://leetcode.com/problems/non-decreasing-array/
 function checkPossibility(nums) {
-    //for item in nums
-      // if item > nums[i+1]
-        // check the item and sort it in right place
-        // for item-new in new arr
-          // if item-new > nums[i+1] 
-          //return false
+  // for num in nums
+  // in num[i] && num[i+1] && num[i+2] find min & max
+  //if (min != num[i] || max != num[i + 2])
+  // if !reachtolimmit 
+  // if min = num[i+2] 
+  //remove min && set reachtolimmit to true
+  // if max = num[i]
+  //remove max & set reachtolimmit to true
+  //else 
+  //return false
+  // return true
+  let reachToLimit = false;
 
-    //return true
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (i === nums.length - 2) {
+      if (nums[i] > nums[i + 1]) {
+        return !reachToLimit;
+      }
+    } else {
+      const min = Math.min(nums[i], nums[i + 1], nums[i + 2]);
+      const max = Math.max(nums[i], nums[i + 1], nums[i + 2]);
 
-    let reachToLimit = false;
-
-    for(let i =0; i< nums.length; i++) {
-      if(nums[i] > nums[i+1]) {
+      if (min != nums[i] || max != nums[i + 2]) {
         if (!reachToLimit) {
           reachToLimit = true;
-          let indexOfChangingItem = i;
-          for(let j = i+1; j<nums.length; j++) {
-            if (nums[indexOfChangingItem] > nums[j]) {
-              nums.splice(i, 2, nums[j], nums[indexOfChangingItem]);
-              indexOfChangingItem++;
-              j++;
-            }
+          if (min === nums[i + 2]) {
+            nums.splice(i + 2, 1);
+          } else if (max === nums[i]) {
+            nums.splice(i, 1);
+          } else {
+            nums.splice(i + 1, 1);
           }
+          i--;
+          continue;
         }
-        return false
+        return false;
       }
     }
-    return true;
-};
+  }
+  return true;
+}
 
 
 console.log('(true): ', checkPossibility([4,2,3,4,5,6]));
