434f3405ec0b5a9261f9dfdbf512c4939a35fda6
marzieh moghtaderi <marzzy.m@gmail.com>
2020-12-25 12:08:02 +0330

    leetcode(WIP): 1365. How Many Numbers Are Smaller Than the Current Number

---
+++ javascript-problems-02/how-many-numbers-are-smaller-than-the-current-number/smallerNumbersThanCurrent.js
@@ -0,0 +1,16 @@
+function smallerNumbersThanCurrent(nums) {
+  //let result = [];
+  //let smallerCounter = 0
+  //let mapedNums = new map ([numIndex, num])
+  //* find min or mins in nums
+  //find keys that value is min
+  //for all keys result[key] = smallerCounter
+  //omit mins from nums and add mins counts to smallerCounter
+  //go to * line till nums.length > 1
+  //if nums.length <= 1 
+  //
+};
+
+console.log('([4,0,1,1,3]): ', smallerNumbersThanCurrent([8,1,2,2,3]));
+// console.log('([2,1,0,3]): ', smallerNumbersThanCurrent([6,5,4,8]));
+// console.log('([0,0,0,0]): ', smallerNumbersThanCurrent([7,7,7,7]));
