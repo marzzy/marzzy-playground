d4cd4828f8be6d628b8cb7dc61065bb1c7f220a6
marzzy mogh <marzzy.m@gmail.com>
2020-12-26 23:33:30 +0330

    leetcode(WIP-1365-2nd): How Many Numbers Are Smaller Than the Current Number
    actually solve the problem, I just wondering the others solutions

--- javascript-problems-02/how-many-numbers-are-smaller-than-the-current-number/smallerNumbersThanCurrent.js
+++ javascript-problems-02/how-many-numbers-are-smaller-than-the-current-number/smallerNumbersThanCurrent.js
@@ -1,16 +1,29 @@
+// Runtime: 92 ms, faster than 83.50 % of JavaScript online submissions for How Many Numbers Are Smaller Than the Current Number.
+// Memory Usage: 41.9 MB, less than 16.08 % of JavaScript online submissions for How Many Numbers Are Smaller Than the Current Number.
 function smallerNumbersThanCurrent(nums) {
-  //let result = [];
-  //let smallerCounter = 0
-  //let mapedNums = new map ([numIndex, num])
-  //* find min or mins in nums
-  //find keys that value is min
-  //for all keys result[key] = smallerCounter
-  //omit mins from nums and add mins counts to smallerCounter
-  //go to * line till nums.length > 1
-  //if nums.length <= 1 
-  //
+  //hashmap > [uniqSortedItem(from min to max), array of its indexes]
+  //smallerCounter = 0
+  //baraye har uniqSortedItem 
+    //be ezaye har indexi
+      //result[index] = smallerCounter
+    //smallerCounter += index.lenght
+
+  let uniqSortedItemsMap = {} ,smallerCounter = 0, result = [];
+
+  nums.forEach((num, numIndex) => {
+    uniqSortedItemsMap[num] = uniqSortedItemsMap[num]
+      ? [...uniqSortedItemsMap[num], numIndex]
+      : [numIndex]
+  });
+
+  Object.values(uniqSortedItemsMap).forEach(indexArray => {
+    indexArray.forEach(numIndex => result[numIndex] = smallerCounter);
+    smallerCounter += indexArray.length;
+  });
+
+  return result;
 };
 
 console.log('([4,0,1,1,3]): ', smallerNumbersThanCurrent([8,1,2,2,3]));
-// console.log('([2,1,0,3]): ', smallerNumbersThanCurrent([6,5,4,8]));
-// console.log('([0,0,0,0]): ', smallerNumbersThanCurrent([7,7,7,7]));
+console.log('([2,1,0,3]): ', smallerNumbersThanCurrent([6,5,4,8]));
+console.log('([0,0,0,0]): ', smallerNumbersThanCurrent([7,7,7,7]));
