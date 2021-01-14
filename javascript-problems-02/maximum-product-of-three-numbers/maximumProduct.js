64241bf1c03363e3aaab468592a1b583b2ad0ca9
marzieh moghtaderi <marzzy.m@gmail.com>
2021-01-14 17:23:42 +0330

    leetcode(done-628-2nd): add o(n) solution

--- javascript-problems-02/maximum-product-of-three-numbers/maximumProduct.js
+++ javascript-problems-02/maximum-product-of-three-numbers/maximumProduct.js
@@ -1,9 +1,36 @@
-function maximumProduct(nums) {
-  nums.sort((a,b)=> a < b ? 1 : -1);
-  const positive = nums[0]*nums[1]*nums[2];
-  const positiveAndNegative = nums[0]*nums[nums.length -2]*nums[nums.length -1];
+// first solution
+// O(nlogn)
+// function maximumProduct(nums) {
+//   nums.sort((a,b)=> a < b ? 1 : -1);
+//   const positive = nums[0]*nums[1]*nums[2];
+//   const positiveAndNegative = nums[0]*nums[nums.length -2]*nums[nums.length -1];
 
-  return Math.max(positive, positiveAndNegative);
+//   return Math.max(positive, positiveAndNegative);
+// };
+
+// 2nd solution
+// O(n)
+function maximumProduct(nums) {
+  let min1, min2;
+  let max1, max2, max3;
+
+  nums.forEach(num => {
+    if(num <= min1 || min1 === undefined) {
+      [ min1 , min2 ] = [num , min1];
+    } else if (num < min2 || min2 === undefined) {
+      min2 = num;
+    }
+
+    if(num >= max1 || max1 === undefined) {
+      [ max1, max2, max3 ] = [ num, max1, max2 ];
+    } else if ( num >= max2 || max2 === undefined ) {
+      [ max1, max2, max3 ] = [ max1 ,num, max2 ];
+    } else if ( num > max3 || max3 === undefined) {
+      max3 = num;
+    }
+  })
+
+  return Math.max(max1*max2*max3, min1*min2*max1);
 };
 
 console.log('(6): ', maximumProduct([1,2,3]));
