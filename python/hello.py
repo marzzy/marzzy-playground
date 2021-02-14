b0c0dc35c7a8e2b5e6070096fd480a3562b44b2f
marzieh moghtaderi <marzzy.m@gmail.com>
2021-02-14 08:10:20 +0330

    cs50: add basic python practise

---
+++ python/hello.py
@@ -0,0 +1,17 @@
+x = 2
+y = 5
+
+if x>y:
+  print('hi')
+elif True:
+  print('bye')
+else: 
+  print('i dont know')
+
+counter = 3
+while counter > 0:
+  print("cough")
+  counter -= 1
+
+for i in range(3):
+  print("cough again")
