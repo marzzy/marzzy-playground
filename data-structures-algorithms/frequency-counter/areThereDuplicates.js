4d23def5fadb19c4403dc0c23d35d3192295e9be
marzzy mogh <marzzy.m@gmail.com>
2021-04-30 13:32:26 +0430

    tutorial(DS&As): frequency counter

---
+++ data-structures-algorithms/frequency-counter/areThereDuplicates.js
@@ -0,0 +1,22 @@
+function areThereDuplicates(arr) {
+    // inputs: arr: array
+    // output : boolean
+
+    // 1. make an frequency object from arr items
+    let frequencyObj = {}
+    for(item of arr) {
+        // 1.a. if there is already an item in object, return true
+        if(frequencyObj[item]) {
+            return true;
+        }
+        frequencyObj[item] = true;
+    }
+
+    // 2. if there isnt any of items in object that have more than one frequency return false
+    return false;
+}
+
+console.log('false: ',areThereDuplicates([1, 2, 3]));
+console.log('true: ',areThereDuplicates([1, 2, 2]));
+console.log('true: ',areThereDuplicates(['a', 'b', 'c', 'a']));
+console.log('true: ',areThereDuplicates(['a', 'b', 'a']));
