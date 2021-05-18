77756769efcfdd77304e580cf81ed7ceef3d7f3f
marzieh moghtaderi <marzzy.m@gmail.com>
2021-05-18 12:36:32 +0430

    tutorial(DS&As): solve flatten exercise with recurcion

--- data-structures-algorithms/recurcion/flatten.js
+++ data-structures-algorithms/recurcion/flatten.js
@@ -1,10 +1,18 @@
-function flat(arr) {
+function flat(mainArr) {
   let result = [];
 
   function setArrResult(arr) {
-    
+    for (item of arr) {
+      if (!Array.isArray(item)) {
+        result.push(item);
+      } else {
+        setArrResult(item);
+      }
+    }
   }
-  setResult(arr);
+
+  setArrResult(mainArr);
+  return result;
 }
 
-console.log('[1,2,3,"hi",4,5,6] : ', flat([[[1,2],[3,["hi"]],4],5,6]))
+console.log('[7,1,2,3,"hi",4,5,6] : ', flat([7,[[1,2],[3,["hi"]],4],5,6]));
