f3be81119ef02d59fdbded962b7dc9aff3bf5c20
marzieh moghtaderi <marzzy.m@gmail.com>
2021-07-14 19:02:15 +0430

    tutorial(DS&As): add 2nd way of implementing insertion sort

--- data-structures-algorithms/sorting-algorithms/insertionSort.js
+++ data-structures-algorithms/sorting-algorithms/insertionSort.js
@@ -1,32 +1,48 @@
+// function insertionSort(arr) {
+//   // loop over arr from 2 item(i)
+//   for(let outerCounter = 1; outerCounter<arr.length; outerCounter++) {
+//     // loop over arr from the i-1 till 0 (j)
+//     let indexofCandidate = false;
+//     for(let innerCounter = outerCounter-1; innerCounter > -1; innerCounter--) {
+//       // let indexOdCandidate = false;
+//       // if arr[j] < arr[i]
+//       if(arr[innerCounter] > arr[outerCounter]) {
+//         // indexofCandidate = j
+//         indexofCandidate = innerCounter;
+//       }
+//     }
+//     // if(typeof indexOdCandidate !== 'boolean')
+//     if(typeof indexofCandidate !== 'boolean') {
+//       // const candidateVal = delete arr[j];
+//       // NOTICE: delete make an empty spot which lead some difficulty
+//       // arr.splice(i,0,candidateVal)
+//       const candidateVal = arr[outerCounter];
+//       arr.splice(outerCounter, 1);
+//       arr.splice(indexofCandidate, 0, candidateVal);
+//     }
+//   }
+//   return arr;
+// }
+
 function insertionSort(arr) {
-  // loop over arr from 2 item(i)
   for(let outerCounter = 1; outerCounter<arr.length; outerCounter++) {
-    // loop over arr from the i-1 till 0 (j)
-    let indexofCandidate = false;
-    for(let innerCounter = outerCounter-1; innerCounter > -1; innerCounter--) {
-      // let indexOdCandidate = false;
-      // if arr[j] < arr[i]
-      if(arr[innerCounter] > arr[outerCounter]) {
-        // indexofCandidate = j
-        indexofCandidate = innerCounter;
+    const item = arr[outerCounter];
+    for(let innerCounter = outerCounter-1; innerCounter > -2; innerCounter--) {
+      if(arr[innerCounter] && arr[innerCounter] > item) {
+        arr[innerCounter+1] = arr[innerCounter];
+      } else {
+        arr[innerCounter+1] = item;
+        break;
       }
-    }
-    // if(typeof indexOdCandidate !== 'boolean')
-    if(typeof indexofCandidate !== 'boolean') {
-      // const candidateVal = delete arr[j];
-      // NOTICE: delete make an empty spot which lead some difficulty
-      // arr.splice(i,0,candidateVal)
-      const candidateVal = arr[outerCounter];
-      arr.splice(outerCounter, 1);
-      arr.splice(indexofCandidate, 0, candidateVal);
     }
   }
   return arr;
 }
 
-console.log(insertionSort([0,0,0]));
-console.log(insertionSort([1,2,3]));
-console.log(insertionSort([6,5,4]));
+// console.log(insertionSort([0,0,0]));
+// console.log(insertionSort([1,2,3]));
+// console.log(insertionSort([6,5,4]));
+//TODO: check whats wrong with this item ??
 console.log(insertionSort([25,0,-1,10,2]));
-console.log(insertionSort([25,0,7,10,2]));
-console.log(insertionSort([0,2,3,4,5,70,10]));
+// console.log(insertionSort([25,0,7,10,2]));
+// console.log(insertionSort([0,2,3,4,5,70,10]));
