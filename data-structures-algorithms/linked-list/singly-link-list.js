336f5483240cd8399c87e4fe2d84e037bbd22db4
marzieh moghtaderi <marzzy.m@gmail.com>
2021-09-12 17:12:49 +0430

    feat(DS&As): implement pop/shift/unshift/get/set/insert methods to singly link list

--- data-structures-algorithms/linked-list/singly-link-list.js
+++ data-structures-algorithms/linked-list/singly-link-list.js
@@ -1,8 +1,8 @@
 class SNode {
   value = null;
-  nextNode = undefined;
+  next = null;
 
-  constructor(value , nextNode) {
+  constructor(value , nextNode = null) {
     this.setValue(value);
     this.setNextNode(nextNode);
   }
@@ -11,10 +11,10 @@ class SNode {
     return this.value;
   }
   getNextNode() {
-    return this.nextNode;
+    return this.next;
   }
   setNextNode(nextNode) {
-    this.nextNode = nextNode;
+    this.next = nextNode;
   }
   setValue(value) {
     this.value = value;
@@ -40,13 +40,17 @@ class SinglyLinkList {
   setTail(tail) {
     this.tail = tail;
   }
-  increaseLengthByOne() {
-    this.length++;
+  setLength(length) {
+    this.length = length;
+  }
+  increaseLength(additionalValue = 1) {
+    this.length = this.length + additionalValue;
   }
   getLength() {
     return this.length;
   }
-  push(newNode) {
+  push(newNodeValue) {
+    const newNode = new SNode(newNodeValue)
     if(this.getHead()) {
       this.getTail().setNextNode(newNode);
       this.setTail(newNode);
@@ -54,7 +58,97 @@ class SinglyLinkList {
       this.setHead(newNode);
       this.setTail(newNode);
     }
-    this.increaseLengthByOne();
+    this.increaseLength();
+
+    return this;
+  }
+
+  pop() {
+    let theNewTail = this.getHead();
+    let currentTail = this.getTail();
+
+    for(let i = 1; i < this.length-1; i++) {
+      theNewTail = theNewTail.getNextNode();
+    }
+    
+    if(this.getLength() <= 1) {
+      this.setHead(null);
+      this.setTail(null);
+      this.setLength(0);
+    } else {
+      theNewTail.setNextNode(null);
+      this.setTail(theNewTail);
+      this.increaseLength(-1);
+    }
+    return currentTail;
+  }
+
+  shift() {
+    let currentHead = this.getHead();
+    
+    if(this.getLength() <= 1) {
+      this.setHead(null);
+      this.setTail(null);
+      this.setLength(0);
+    } else {
+      let newHead = this.getHead().getNextNode();
+      this.increaseLength(-1);
+      this.setHead(newHead);
+    }
+    return currentHead;
+  }
+
+  unshift(newNodeValue) {
+    const newNode = new SNode(newNodeValue, this.getHead());
+    if(!this.getHead()) {
+      this.setTail(newNode);
+    }
+    this.setHead(newNode);
+    this.increaseLength();
+
+    return this;
+  }
+
+  get(index) {
+    if(index <= 0 || index > this.getLength()) {
+      return null;
+    }
+    if(index === this.getLength()) {
+      return this.getTail();
+    }
+    let currentNode = this.getHead();
+    let counter = 1;
+    while(counter < index) {
+      currentNode = currentNode.getNextNode();
+      counter++;
+    }
+    return currentNode;
+  }
+
+  set(nodeIndex, newValue) {
+    let targetNode = this.get(nodeIndex);
+    if (targetNode) {
+      targetNode.setValue(newValue);
+      return true;
+    }
+    return false;
+  }
+
+  insert(nodeIndex, value) {
+    if(nodeIndex < 0 || nodeIndex > this.getLength()) {
+      return false;
+    }
+    if (nodeIndex === 0) {
+      return Boolean(this.unshift(value));
+    }
+    if( nodeIndex === this.getLength()) {
+      return Boolean(this.push(value));
+    }
+    let nodeBeforeThisNode = this.get(nodeIndex-1);
+    const newNode = new SNode(value, nodeBeforeThisNode.getNextNode());
+    nodeBeforeThisNode.setNextNode(newNode);
+    this.increaseLength();
+    return true;
   }
 }
 
--- data-structures-algorithms/linked-list/singly-link-list.test.js
+++ data-structures-algorithms/linked-list/singly-link-list.test.js
@@ -5,7 +5,7 @@ test("test create node without next node value", () => {
     let a = new SNode(32);
 
     expect(a.getValue()).toBe(32);
-    expect(a.getNextNode()).toBe(undefined);
+    expect(a.getNextNode()).toBe(null);
 });
 
 test("test create full option node", () => {
@@ -24,36 +24,246 @@ test("test create singlyLinkList", () => {
 });
 
 test("push new node to empty singlyLinkList", () => {
-    let a = new SNode(32);
+    let a = 32;
     let myList = new SinglyLinkList();
 
     expect(myList.getHead()).toBe(null);
 
     myList.push(a)
 
-    expect(myList.getHead()).toBe(a);
+    expect(myList.getHead().getValue()).toBe(a);
 });
 
 test("push 3 new nodes to a singlyLinkList", () => {
-    let d = new SNode(32);
-    let c = new SNode(12);
-    let b = new SNode(13);
-    let a = new SNode(14);
+    let d = 32;
+    let c = 12;
+    let b = 13;
+    let a = 14;
     let myList = new SinglyLinkList();
 
     myList.push(a);
     myList.push(b);
     myList.push(c);
 
-    expect(myList.getHead().getNextNode().getNextNode().getNextNode()).toBe(undefined);
+    expect(myList.getHead().getNextNode().getNextNode().getNextNode()).toBe(null);
     expect(myList.getLength()).toBe(3);
 
     myList.push(d);
 
-    expect(myList.getHead()).toBe(a);
-    expect(myList.getHead().getNextNode()).toBe(b);
-    expect(myList.getHead().getNextNode().getNextNode()).toBe(c);
-    expect(myList.getHead().getNextNode().getNextNode().getNextNode()).toBe(d);
-    expect(myList.getHead().getNextNode().getNextNode().getNextNode().getNextNode()).toBe(undefined);
+    expect(myList.getHead().getValue()).toBe(a);
+    expect(myList.getHead().getNextNode().getValue()).toBe(b);
+    expect(myList.getHead().getNextNode().getNextNode().getValue()).toBe(c);
+    expect(myList.getHead().getNextNode().getNextNode().getNextNode().getValue()).toBe(d);
+    expect(myList.getHead().getNextNode().getNextNode().getNextNode().getNextNode()).toBe(null);
     expect(myList.getLength()).toBe(4);
+});
+
+test("pop from a singlyLinkList", () => {
+    let c = 12;
+    let b = 13;
+    let a = 14;
+    let myList = new SinglyLinkList();
+
+    myList.push(a);
+    myList.push(b);
+    myList.push(c);
+    let returnedNode = myList.pop();
+
+    expect(myList.getLength()).toBe(2);
+    expect(myList.getTail().getValue()).toBe(b);
+    expect(myList.getTail().getNextNode()).toBe(null);
+    expect(returnedNode.getValue()).toBe(c);
+    expect(returnedNode.getNextNode()).toBe(null);
+
+    returnedNode = myList.pop();
+
+    expect(myList.getLength()).toBe(1);
+    expect(myList.getHead().getValue()).toBe(a);
+    expect(myList.getTail().getValue()).toBe(a);
+    expect(myList.getTail().getNextNode()).toBe(null);
+    expect(returnedNode.getValue()).toBe(b);
+    expect(returnedNode.getNextNode()).toBe(null);
+
+    returnedNode = myList.pop();
+
+    expect(myList.getLength()).toBe(0);
+    expect(myList.getHead()).toBe(null);
+    expect(myList.getTail()).toBe(null);
+    expect(returnedNode.getNextNode()).toBe(null);
+    expect(returnedNode.getValue()).toBe(a);
+
+    returnedNode = myList.pop();
+
+    expect(myList.getLength()).toBe(0);
+    expect(myList.getHead()).toBe(null);
+    expect(myList.getTail()).toBe(null);
+    expect(returnedNode).toBe(null);
+});
+
+test("shift from a singlyLinkList", () => {
+    let c = 12;
+    let b = 13;
+    let a = 14;
+    let myList = new SinglyLinkList();
+
+    myList.push(a);
+    myList.push(b);
+    myList.push(c);
+    let returnedNode = myList.shift();
+
+    expect(myList.getLength()).toBe(2);
+    expect(myList.getHead().getValue()).toBe(b);
+    expect(returnedNode.getValue()).toBe(a);
+
+    returnedNode = myList.shift();
+
+    expect(myList.getLength()).toBe(1);
+    expect(myList.getHead().getValue()).toBe(c);
+    expect(returnedNode.getValue()).toBe(b);
+
+    returnedNode = myList.shift();
+
+    expect(myList.getLength()).toBe(0);
+    expect(myList.getHead()).toBe(null);
+    expect(myList.getTail()).toBe(null);
+    expect(returnedNode.getValue()).toBe(c);
+
+    returnedNode = myList.shift();
+    
+    expect(myList.getLength()).toBe(0);
+    expect(myList.getHead()).toBe(null);
+    expect(myList.getTail()).toBe(null);
+    expect(returnedNode).toBe(null);
+});
+
+test("unshift to a singlyLinkList", () => {
+    let c = 12;
+    let b = 13;
+    let a = 14;
+    let myList = new SinglyLinkList();
+
+    myList.unshift(c);
+
+    expect(myList.getLength()).toBe(1);
+    expect(myList.getHead().getValue()).toBe(c);
+    expect(myList.getTail().getValue()).toBe(c);
+
+    myList.unshift(b);
+
+    expect(myList.getLength()).toBe(2);
+    expect(myList.getHead().getValue()).toBe(b);
+    expect(myList.getHead().getNextNode().getValue()).toBe(c);
+    expect(myList.getTail().getValue()).toBe(c);
+
+    myList.unshift(a);
+
+    expect(myList.getLength()).toBe(3);
+    expect(myList.getHead().getValue()).toBe(a);
+    expect(myList.getHead().getNextNode().getValue()).toBe(b);
+    expect(myList.getHead().getNextNode().getNextNode().getValue()).toBe(c);
+    expect(myList.getTail().getValue()).toBe(c);
+});
+
+test("get a node in singlyLinkList", () => {
+    let c = 12;
+    let b = 13;
+    let a = 14;
+    let myList = new SinglyLinkList();
+    let retunedNode = myList.get(1);
+
+    expect(retunedNode).toBe(null);
+
+    myList.push(a);
+    myList.push(b);
+    myList.push(c);
+    retunedNode = myList.get(1);
+
+    expect(retunedNode.getValue()).toBe(a);
+    expect(retunedNode.getNextNode().getValue()).toBe(b);
+
+    retunedNode = myList.get(2);
+
+    expect(retunedNode.getValue()).toBe(b);
+    expect(retunedNode.getNextNode().getValue()).toBe(c);
+
+    retunedNode = myList.get(3);
+
+    expect(retunedNode.getValue()).toBe(c);
+    expect(retunedNode.getNextNode()).toBe(null);
+
+    retunedNode = myList.get(-3);
+
+    expect(retunedNode).toBe(null);
+
+    retunedNode = myList.get(40);
+
+    expect(retunedNode).toBe(null);
+    
+    retunedNode = myList.get(0);
+
+    expect(retunedNode).toBe(null);
+
+});
+
+test("set a node value in singlyLinkList", () => {
+    let c = 12;
+    let b = 13;
+    let a = 14;
+    let myList = new SinglyLinkList();
+    let retunedValue = myList.set(1, 70);
+
+    expect(retunedValue).toBe(false);
+    
+    retunedValue = myList.set(-1, 70);
+
+    expect(retunedValue).toBe(false);
+
+    myList.push(a);
+    myList.push(b);
+    myList.push(c);
+    retunedValue = myList.set(1, 71);
+
+    expect(retunedValue).toBe(true);
+    expect(myList.get(1).getValue()).toBe(71);
+
+    retunedValue = myList.set(100, 2);
+
+    expect(retunedValue).toBe(false);
+});
+
+test("insert a node in singlyLinkList", () => {
+    let c = 12;
+    let b = 13;
+    let a = 14;
+    let myList = new SinglyLinkList();
+    myList.push(a);
+    myList.push(b);
+    myList.push(c);
+
+    let retunedValue = myList.insert(100, 70);
+
+    expect(retunedValue).toBe(false);
+    
+    retunedValue = myList.insert(-1, 71);
+
+    expect(retunedValue).toBe(false);
+    
+    let beforeInsert1ndNode = myList.get(1);
+    let beforeInsert2ndNode = beforeInsert1ndNode.getNextNode();
+    retunedValue = myList.insert(2, 50);
+
+    expect(retunedValue).toBe(true);
+    expect(myList.get(1)).toBe(beforeInsert1ndNode);
+    expect(myList.get(1).getNextNode().getValue()).toBe(50);
+    expect(myList.get(2).getNextNode()).toBe(beforeInsert2ndNode);
+
+    retunedValue = myList.insert(0, 71);
+
+    expect(retunedValue).toBe(true);
+    expect(myList.getHead().getValue()).toBe(71);
+
+    retunedValue = myList.insert(myList.getLength(), 72);
+
+    expect(retunedValue).toBe(true);
+    expect(myList.getTail().getValue()).toBe(72);
 });
