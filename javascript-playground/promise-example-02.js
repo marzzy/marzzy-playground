c2554549879d96b62e118854d46fd9191adca125
marzzy mogh <marzzy.m@gmail.com>
2022-03-01 23:34:58 +0330

    practise(promises): implement promise all with limitation of firing paralel promises

--- javascript-playground/promise-example-02.js
+++ javascript-playground/promise-example-02.js
@@ -72,3 +72,48 @@ function promiseAllSettledPolyfill (promiseArray) {
 //   23
 // ])
 // .then(console.log);
+
+function promiseAllWithLimitation (promiseArray, limit) {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let isThereNoError = true;
+    let fullfilledPromise = 0;
+    let lastPromiseIndex = limit-1;
+
+    function firePromise(counter) {
+      console.log('counter', counter);
+      promiseArray[counter]
+        .then(result => results[counter] = result)
+        .then(() => {
+          fullfilledPromise++;
+          if (lastPromiseIndex < promiseArray.length - 1 && isThereNoError) {
+            lastPromiseIndex++;
+            firePromise(lastPromiseIndex);
+          }
+        })
+        .catch(error => {
+          isThereNoError = false;
+          reject(error);
+        }).finally(() => {
+          if (fullfilledPromise === promiseArray.length) {
+            resolve(results);
+          }
+        })
+    }
+
+    for(let i = 0; i < limit && isThereNoError; i++) {
+      firePromise(i);
+    }
+  })
+}
+
+// promiseAllWithLimitation([
+//   new Promise(resolve => setTimeout(() => resolve(1), 1500)), // 1
+//   // new Promise((re, reject) => reject('foo')), // foo
+//   new Promise(resolve => setTimeout(() => resolve(2), 2000)), // 2
+//   new Promise(resolve => setTimeout(() => resolve(3), 1000)),  // 3
+//   new Promise(resolve => setTimeout(() => resolve(4), 1200)),  // 4
+//   new Promise(resolve => setTimeout(() => resolve(5), 3000)),  // 5
+//   new Promise(resolve => setTimeout(() => resolve(7), 1400)),  // 6
+// ], 4).then(console.log)
+// .catch(console.log);
